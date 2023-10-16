5c0442436d67b99a939e18781d2607dad437eb70
Meara1179 <xenon1179@gmail.com>
2023-10-17 07:51:45 +1000

    Checkpoint 2

--- IFB102MiniProject/Program.cs
+++ IFB102MiniProject/Program.cs
@@ -1,7 +1,14 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+
+#if !DEBUG
+builder.WebHost.UseUrls("http://*:5000;");
+#endif
 
 var app = builder.Build();
 
@@ -13,7 +20,7 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
--- IFB102MiniProject/wwwroot/js/gameCode.js
+++ IFB102MiniProject/wwwroot/js/gameCode.js
@@ -4,6 +4,7 @@ var playerMaxHP = 150;
 var playerCurrentHP = 150;
 var playerMaxMP = 150;
 var playerCurrentMP = 150;
+var playerNameText = document.getElementById("playerNameText");
 var playerHPText = document.getElementById("playerHPText");
 var playerHPBar = document.getElementById("playerHP");
 var playerMPText = document.getElementById("playerMPText");
@@ -11,16 +12,46 @@ var playerMPBar = document.getElementById("playerMP");
 
 var aiAction = { actionType: "", actionRoll: 0 };
 var aiName = "Enemy";
-var aiMaxHP = 100;
-var aiCurrentHP = 100;
+var aiMaxHP = 150;
+var aiCurrentHP = 150;
+var aiNameText = document.getElementById("aiNameText");
 var aiHPText = document.getElementById("aiHPText");
 var aiHPBar = document.getElementById("aiHP");
 
 var clashResult;
 var turnCount = 1;
+var fightCount = 0;
+var fightStatus = 0;
 
 var battleResultsElement = document.getElementById("battleResults");
 var attackButton = document.getElementById("attack");
+var specialAttackButton = document.getElementById("specialAttack");
+
+function startOfGame() {
+    playerName = sessionStorage.getItem("playerName");
+    playerNameText.innerHTML = `Name: ${playerName}`;
+    playerCurrentHP = playerMaxHP;
+    playerCurrentMP = playerMaxMP;
+    fightCount = 0;
+
+    startOfFight();
+}
+
+function startOfFight() {
+    fightCount++;
+    fightStatus = 0;
+    turnCount = 1;
+    battleResultsElement.innerHTML = "";
+    attackButton.innerHTML = "Attack";
+    playerCurrentMP = playerMaxMP;
+
+    enemyNamePicker();
+    aiNameText.innerHTML = `Name: ${aiName}`;
+    aiCurrentHP = aiMaxHP;
+    specialAttackButton.disabled = false;
+
+    updateStatus();
+}
 
 function ClashCalculator()
 {
@@ -38,13 +69,21 @@ function ClashCalculator()
 
     if (clashWinner == playerName) {
         if (aiAction.actionType == "Guard") {
-            var guardedDamage = playerAction.actionRoll - aiAction.actionRoll;
+            var guardedDamage = math.floor(playerAction.actionRoll - aiAction.actionRoll);
             clashResult = `${playerName} won! ${aiName} guarded and took ${guardedDamage} damage!`;
             aiCurrentHP -= guardedDamage;
+
+            if (playerAction.actionType == "Special Attack") {
+                playerCurrentHP += guardedDamage;
+            }
         }
         else {
             clashResult = `${playerName} won! ${aiName} took ${playerAction.actionRoll} damage!`;
             aiCurrentHP -= playerAction.actionRoll;
+
+            if (playerAction.actionType == "Special Attack") {
+                playerCurrentHP += playerAction.actionRoll;
+            }
         }
     }
     else if (clashWinner == aiName) {
@@ -62,7 +101,7 @@ function ClashCalculator()
         clashResult = "The clash resulted in a draw.";
     }
 
-    addResultText("Turn: " + turnCount + "<br>" + "Player HP: " + playerCurrentHP + "<br>" + "Player Roll: " + playerAction.actionRoll + "<br>" + "AI HP: " + aiCurrentHP + "<br>" +  "AI Roll: " + aiAction.actionRoll + "<br>" + clashResult + "<br>");
+    addResultText(`Turn: ${turnCount} <br>${playerName}'s Roll: ${playerAction.actionRoll} <br>${aiName}'s Roll: ${aiAction.actionRoll} <br>${clashResult} <br>`);
     turnCount++;
     endOfTurn();
 }
@@ -74,11 +113,30 @@ function addResultText(resultText) {
 }
 
 function playerAttack() {
-    playerAction.actionType = "Attack";
-    playerAction.actionRoll = Math.floor(Math.random() * (12 - 8 + 1) + 8)
+    if (fightStatus == 0) {
+        playerAction.actionType = "Attack";
+        playerAction.actionRoll = Math.floor(Math.random() * (12 - 8 + 1) + 8)
 
-    aiAttack();
-    ClashCalculator();
+        aiAttack();
+        ClashCalculator();
+    }
+    else if (fightStatus == 1) {
+        startOfFight();
+    }
+    else if (fightStatus == 2) {
+        startOfGame();
+    }
+}
+
+function playerSpecialAttack() {
+    if (playerCurrentMP >= 25) {
+        playerCurrentMP -= 25;
+        playerAction.actionType = "Special Attack";
+        playerAction.actionRoll = Math.floor(Math.random() * (20 - 15 + 1) + 15)
+
+        aiAttack();
+        ClashCalculator();
+    }
 }
 
 function aiAttack() {
@@ -87,19 +145,34 @@ function aiAttack() {
 }
 
 function endOfTurn() {
+    if (playerCurrentHP > playerMaxHP) {
+        playerCurrentHP = playerMaxHP;
+    }
+    if (playerCurrentMP < 25) {
+        specialAttackButton.disabled = true;
+    }
+    else if (playerCurrentMP >= 25) {
+        specialAttackButton.disabled = false;
+    }
+
     if (playerCurrentHP <= 0) {
         playerCurrentHP = 0;
-        playerHPBar.style.width = "0%";
-        attackButton.disabled = true;
-        addResultText(`${playerName} lost... Game Over`)
+        attackButton.innerHTML = "Retry";
+        fightStatus = 2;
+        addResultText(`${playerName} lost... Game Over <br> You beat ${fightCount - 1} enemies.`)
+        specialAttackButton.disabled = true;
     }
     else if (aiCurrentHP <= 0) {
         aiCurrentHP = 0;
-        aiHPBar.style.width = "0%";
-        attackButton.disabled = true;
-        addResultText(`${aiName} was defeated! You win!!!`)
+        attackButton.innerHTML = "Next Fight";
+        fightStatus = 1;
+        addResultText(`${aiName} was defeated! You win!!! <br> You've beaten ${fightCount} enemies.`)
+        specialAttackButton.disabled = true;
     }
+    updateStatus();
+}
 
+function updateStatus() {
     playerHPText.innerHTML = `HP: ${playerCurrentHP} / ${playerMaxHP}`
     playerHPBar.style.width = `${(playerCurrentHP / playerMaxHP) * 100}%`;
     playerMPText.innerHTML = `MP: ${playerCurrentMP} / ${playerMaxMP}`
@@ -108,3 +181,33 @@ function endOfTurn() {
     aiHPText.innerHTML = `HP: ${aiCurrentHP} / ${aiMaxHP}`
     aiHPBar.style.width = `${(aiCurrentHP / aiMaxHP) * 100}%`;
 }
+
+function enemyNamePicker() {
+    var enemyNameNumber = Math.floor(Math.random() * (7 - 1 + 1) + 1)
+
+    switch (enemyNameNumber) {
+        case 1:
+            aiName = "Grimetooth";
+            break;
+        case 2:
+            aiName = "Foulpod";
+            break;
+        case 3:
+            aiName = "Netherpaw";
+            break;
+        case 4:
+            aiName = "Flamebeast";
+            break;
+        case 5:
+            aiName = "Nethermorph";
+            break;
+        case 6:
+            aiName = "Plaguecreep";
+            break;
+        case 7:
+            aiName = "Sarah"
+            break;
+    }
+}
+
+window.onload = startOfGame();
